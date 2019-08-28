18838dac69d07a24bb2cb5d70e979ef7bdec26ee
IVB107 <ianvb107@gmail.com>
2019-08-28 16:00:32 -0500

    Created SongList, NewSongForm. Implemented useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SongList from './components/SongList';
 
 
 function App() {
   return (
     <div className="App">
+      <SongList />
     </div>
   );
 }
--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import uuid from 'uuid/v1'
+import NewSongForm from './NewSongForm';
 
-export default function SongList() {
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Everything in its Right Place', id: 1 },
+    { title: 'Solitude is Bliss', id: 2 },
+    { title: "Oh Sheit it's X", id: 3 }
+  ])
+  const addSong = title => {
+    setSongs([...songs, {title, id: uuid()}])
+  }
+  useEffect(() => {
+    console.log('useEffect hook ran', songs)
+  })
   return (
-    <div>
-      
+    <div className="song-list">
+      <ul>
+        {songs.map(song => ( <li key={song.id}>{song.title}</li> ) )}
+      </ul>
+      <button onClick={addSong}>Add New Song</button>
+      <NewSongForm addSong={addSong}/>
     </div>
   )
 }
+
+export default SongList
